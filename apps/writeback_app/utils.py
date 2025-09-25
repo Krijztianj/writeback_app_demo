7e4f5ee63107fd1f244375f76ecce8be0c0ac4e4
Krijztianj <kristian.johannesen@outlook.dk>
2025-09-25 13:27:50 +0200

    Update utils.py

--- apps/writeback_app/utils.py
+++ apps/writeback_app/utils.py
@@ -5,6 +5,31 @@ cfg = Config()
 WAREHOUSE_HTTP_PATH = "/sql/1.0/warehouses/5eced0d6af754723"
 TABLE_NAME = "app_demos.writeback.products"
 
+products_ddl = f"""
+CREATE CATALOG IF NOT EXISTS app_demos;
+CREATE SCHEMA IF NOT EXISTS app_demos.writeback;
+
+CREATE TABLE IF NOT EXISTS app_demos.writeback.products AS 
+
+with product as (
+select distinct product 
+
+from samples.bakehouse.sales_transactions
+),
+cost_price as (
+select 
+product as product_name 
+, CAST(FLOOR(rand() * 10) + 1 AS DECIMAL(10,2)) as cost_price
+from product )
+
+select 
+row_number() OVER(partition by 1 order by 1) AS product_id
+, product_name
+, cost_price
+, cost_price*2 as sales_price
+from cost_price
+"""
+
 def get_connection():
     return sql.connect(
         server_hostname=cfg.host,
@@ -14,10 +39,8 @@ def get_connection():
 
 def init_products_table(conn):
     """Create products table if it does not exist."""
-    with open("resources/products.sql", "r") as f:
-        sql_script = f.read()
     with conn.cursor() as cursor:
-        cursor.execute(sql_script)
+        cursor.execute(products_ddl)
 
 def read_table(conn):
     query = f"""
